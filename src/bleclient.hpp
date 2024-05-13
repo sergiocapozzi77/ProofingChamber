4c09f0e600c546c7b4bb90daccfdafa3e2fea916
Sergio Capozzi <sergiocapozzi@outlook.com>
2024-05-13 21:48:47 +0100

    min temperature

--- src/bleclient.hpp
+++ src/bleclient.hpp
@@ -6,19 +6,20 @@
 class MyBLEClient
 {
 
+ 
+public:
     // The remote service we wish to connect to.
     BLEUUID serviceUUID;
     // The characteristic of the remote service we are interested in.
     BLEUUID charUUID;
-
-public:
-    bool doConnect = false;
+   bool doConnect = false;
     bool doScan = false;
     BLERemoteCharacteristic *pRemoteCharacteristic;
     BLEAdvertisedDevice *myDevice;
     bool connected = false;
     bool ConnectToServer();
     void Setup();
+    void Loop();
 };
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include <Arduino.h>
 #include "blemanager.hpp"
 
+unsigned long previousMillis = 0; // will store last time LED was updated
+const long interval = 1000;
+const uint8_t RELAY_PIN = 1;
+const float desiredTemperature = 18.0;
+const long minTimeBetweenOn = 60000;
+unsigned long previousOn = -minTimeBetweenOn;
+
 BLEManager bleManager;
 
 void setup()
@@ -9,9 +16,29 @@ void setup()
   Serial.println();
   Serial.println(F("Proofing Chamber starting"));
   bleManager.SetupBLE();
+
+  pinMode(RELAY_PIN, OUTPUT);
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval)
+  {
+    // save the last time you blinked the LED
+    previousMillis = currentMillis;
+
+    float currentTemperature = 11;
+
+    if (currentTemperature > desiredTemperature && currentMillis - previousOn >= minTimeBetweenOn)
+    {
+      digitalWrite(RELAY_PIN, HIGH);
+      previousOn = currentMillis;
+    }
+    else
+    {
+      digitalWrite(RELAY_PIN, LOW);
+    }
+  }
 }
